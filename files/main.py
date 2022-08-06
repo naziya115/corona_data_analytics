0f2825e31fe2ac7945c2634233f828822c2aa93f
Naziya <naziyazhanabaeva@gmail.com>
2022-08-06 22:32:07 +0600

    map update

--- files/main.py
+++ files/main.py
@@ -1,1 +1,10 @@
-print('hi')
+import folium
+
+marker_map = folium.Map(location=[42.36021388083161, -71.09404198274717], zoom_start=12, tiles='Stamen Terrain')
+folium.Marker(
+    location=[42.36021388083161, -71.09404198274717],
+    popup='massachusetts institute of technology',
+    icon=folium.Icon(icon='cloud'),
+).add_to(marker_map)
+
+marker_map.save('mit_map.html')
